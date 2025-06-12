8fff90205c5cb6cb01c5cb09fbb5d754f7c94ef9
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-12 15:59:33 +0600

    integrated mongoDB to server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,6 +7,35 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqhhjdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+      await client.connect();
+
+    
+    
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
