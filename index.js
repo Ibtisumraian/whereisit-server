45e7584e7726723ae14ed792724aff551c22cfdc
Ibtisumraian <ibtisumraian@gmail.com>
2025-06-17 01:37:36 +0600

    integrated JWT token

--- index.js
+++ index.js
@@ -2,14 +2,38 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken')
+const cookieParser =require('cookie-parser')
 const port = process.env.PORT || 5000
 
 
-app.use(cors())
+app.use(cors({
+  origin: ['https://whereisit-app.web.app', 'http://localhost:5173'],
+  credentials: true,
+}))
 app.use(express.json())
+app.use(cookieParser());
 
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token
+  console.log("inside the verify token middleware", token)
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" })
+  }
 
+  // verify token
+
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" })
+    }
+    console.log(decoded);
+    req.decoded = decoded
+    next()
+  })
+  
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqhhjdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,6 +54,19 @@ async function run() {
     const recoveredCollection = client.db('whereIsItDB').collection('recovered')
       
 
+    app.post('/jwt', async (req, res) => {
+      const userData = req.body
+      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, { expiresIn: '2h' })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      res.send({ success: true })
+    })
+
+
+    
     app.post('/items', async (req, res) => {
       const item = req.body
       const result = await itemsCollection.insertOne(item)
@@ -37,12 +74,11 @@ async function run() {
     })
     
 
-    app.get('/items', async (req, res) => {
+    app.get('/items', verifyToken, async (req, res) => {
       const { title } = req.query;
       const query = {};
-
       if (title) {
-        query.title = { $regex: title, $options: 'i' };
+        query.Search_input = { $regex: title, $options: 'i' };
       }
 
       try {
@@ -54,8 +90,9 @@ async function run() {
     })
 
 
-    app.get('/items/:id', async(req, res) => {
+    app.get('/items/:id', verifyToken, async(req, res) => {
       const id = req.params.id
+      console.log('inside application cookie', req.cookies);
       const query = { _id: new ObjectId(id) }
       const result = await itemsCollection.findOne(query)
       res.send(result)
@@ -64,7 +101,7 @@ async function run() {
     
 
 
-    app.get('/recent', async (req, res) => {
+    app.get('/recent', verifyToken, async (req, res) => {
       try {
         const result = await itemsCollection.find().sort({ recent_date: -1  }).limit(6).toArray();
         res.send(result);
@@ -95,8 +132,11 @@ async function run() {
 
 
 
-    app.get('/recovered/:email', async (req, res) => {
+    app.get('/recovered/:email', verifyToken, async (req, res) => {
       const email = req.params.email
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
       const query = { recovered_mail: email };
       const result = await recoveredCollection.find(query).toArray();
       res.send(result)
